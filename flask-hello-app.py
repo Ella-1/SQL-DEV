9b681cb1ec29721433484755e97a094a3dbc95c8
Bata <es9897288@.com>
2022-07-27 04:56:15 -0700

    task1

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mypassword@localhost:5432/mysql'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return 'hell world!'
+
+
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
